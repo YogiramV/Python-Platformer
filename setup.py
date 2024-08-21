5dac3a500c1a5d8fbdf8063b000c5df864850081
Yogiram <75132048+YogiramV@users.noreply.github.com>
2024-08-21 19:02:09 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+    name='python-platformer',
+    version='0.1',
+    packages=find_packages(),
+    author='Yogiram',
+    author_email='yogiramkv@gmail.com',
+    description='A simple pygame package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
